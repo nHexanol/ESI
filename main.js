e13c4aaedd45eefd4cb291dd204b6c0cc7cb030b
ChocolateBiscut is dying <38654142+Justin2528@users.noreply.github.com>
2021-08-16 00:52:41 +0800

    .tl (#5)
    * Update main.js
    
    * Update main.js

--- main.js
+++ main.js
@@ -1381,7 +1381,38 @@ else if (cmd == "p") {
 		console.log(error);
 	});
 }
+else 	if(cmd == "tl"){
+		const playerls = new Discord.MessageEmbed()
+		var output = "";
+		playerls.setColor('#009eff')
 
+		man = 0;
+		if(!args[0]) return message.channel.send("Please provide a guild's tag (no name XD)")
+		let gprefix = args[0].toLowerCase()
+		let terrs = []
+		fetch('https://athena.wynntils.com/cache/get/territoryList')
+		.then(response => response.json())
+		.then(json => {
+			
+			for (var g in json.territories) {
+				let guprefix = json.territories[g].guildPrefix.toLowerCase()
+				if(guprefix === gprefix){
+					man++
+				terrs.push(`${man}. ${json.territories[g].territory}`);
+				}
+			}
+			if(terrs.length === 0) {
+				terrs.push(`${gprefix} has 0 territories.`);
+				}
+				for (var m in terrs) {
+					output = output.concat(`${terrs[m]}\n`)
+				}
+				playerls.setTitle(`Territory list for ${gprefix}`)
+				playerls.setDescription(`\`\`\`\n${output}\n\`\`\``)
+			   playerls.setFooter(`${gprefix} has ${man} territories`)
+			   message.channel.send(playerls)
+		})
+	}
 else if (cmd == "gs") {
 	if (args.length == 0) var guild = "Empire+of+Sindria";
 	else if (args.length != 0) var guild = message.content.replace(`${prefix}${cmd} `, '').replace(/ /g, "+");
