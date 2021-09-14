c1fdfd40c1b0ba276d2d89ca798300b99d3ae379
nHexanol <72498523+nHexanol@users.noreply.github.com>
2021-09-14 23:51:17 +0700

    Delete wynncraft.js

--- wynncraft.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-exports.player = function player(username) {
-    fetch(`https://api.wynncraft.com/v2/player/${username}/stats`)
-				.then(res => res.json())
-				.then(function (json) {
-                    return json;
-})
-                .catch(console.error)
-}
