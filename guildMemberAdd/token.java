326f8f06f545108ddd40469cb7275f5a081505d4
nHexanol <72498523+nHexanol@users.noreply.github.com>
2021-11-04 11:15:22 +0700

    Update token.java

--- guildMemberAdd/token.java
+++ guildMemberAdd/token.java
@@ -9,7 +9,7 @@ public class token
     private static String token;
     
     static {
-        token = "ODIwMDA5Mzc3MDc1MTY3Mjkz.YEu7Pg.d9uXW1CelZL3lpi38WiOoL7ZR9I";
+        token = "";
     }
 
     public static String getToken() {
