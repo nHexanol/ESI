0854f95bd84d4d02a4ae92c476701f8165ff824c
Jeff Li <zefeng@student.unimelb.edu.au>
2021-11-09 14:09:18 +1100

    update tomeroll

--- slashes/tome/roll.js
+++ slashes/tome/roll.js
@@ -1,17 +1,31 @@
 const { Util } = require("discord.js");
+const { Collection } = require("@discordjs/collection");
+const { createOption } = require("../../interactionHandler");
 
 module.exports = {
     name: 'roll',
     description: 'Rolls for Tomes',
-    callback: function tomeroll(client, interaction, options) {
-        num = 5; // update!
+    options: {
+        number: createOption('INTEGER', 'Numnber of tomes to roll')
+    },
+    callback: async function tomeroll(client, interaction, options) {
+        if (options.num <= 0) {
+            interaction.reply({content: "`number` must be greater than 0!", ephemeral: true});
+            return;
+        } 
 
-        if (isNaN(num)) return;
+        const guild = await interaction.client.guilds.fetch('554418045397762048');
+        const guildMembers = await guild.members.fetch();
+        const citizens = guildMembers.filter(m => m.roles.cache.get('554889169705500672'));
 
-        members = client.guilds.cache.get('554418045397762048').roles.cache.get('897320706973499452').members;
+        const infoChannel = await guild?.channels.fetch('852304243595411528');
+        const tomeMessage = await infoChannel?.messages.fetch('906986707591786557');
+        const reactionUsers = await tomeMessage?.reactions.resolve('📚').users.fetch();
 
-        chosen = members.random(num).filter(Boolean).map(m => Util.escapeMarkdown(m.displayName));
+        const eligible = citizens.intersect(reactionUsers);
 
-        interaction.reply("Warning: As of now the command does not produce truly random selections because of caching.\n\n" + chosen.join('\n'));
+        chosen = eligible.random(num).filter(Boolean).map(m => Util.escapeMarkdown(m.displayName));
+
+        interaction.reply(chosen.join('\n'));
     }
 }
